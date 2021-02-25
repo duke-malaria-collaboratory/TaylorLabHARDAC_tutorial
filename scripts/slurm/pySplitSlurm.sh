f618c043ec2f18db1a89691d1f794ed6ef7015e1
Joe Saelens <jws48@duke.edu>
2021-02-24 23:06:21 -0500

    Update pySplitSlurm.sh

--- scripts/slurm/pySplitSlurm.sh
+++ scripts/slurm/pySplitSlurm.sh
@@ -14,8 +14,8 @@ module load python/3.7.4-gcb01
 module load cutadapt/2.3-gcb01 
 
 echo "$SLURM_ARRAY_TASK_ID"
-pair1=$(ls /data/taylorlab/jws48/avatar/reads/1/*.fastq.gz |sort| sed -n ${SLURM_ARRAY_TASK_ID}p)
-pair2=$(ls /data/taylorlab/jws48/avatar/reads/2/*.fastq.gz |sort| sed -n ${SLURM_ARRAY_TASK_ID}p)
+pair1=$(ls /data/taylorlab/${USER}/HARDAC_tutorial/data/reads/amplicon/1/*.fastq.gz |sort| sed -n ${SLURM_ARRAY_TASK_ID}p)
+pair2=$(ls /data/taylorlab/${USER}/HARDAC_tutorial/data/reads/amplicon/2/*.fastq.gz |sort| sed -n ${SLURM_ARRAY_TASK_ID}p)
 
 refdir="/data/taylorlab/${USER}/HARDAC_tutorial/data/refs"
 out="/data/taylorlab/${USER}/splitReadsResults"
